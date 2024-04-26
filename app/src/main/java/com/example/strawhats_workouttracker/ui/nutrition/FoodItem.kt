fd1ef359cd9f7fe9c5564560c87777645f03de3e
mikewm2001 <michaelwmei09@gmail.com>
2024-04-26 02:39:35 -0400

    Added updating of nutrition information in response to editWeight editText

--- app/src/main/java/com/example/strawhats_workouttracker/ui/nutrition/FoodItem.kt
+++ app/src/main/java/com/example/strawhats_workouttracker/ui/nutrition/FoodItem.kt
@@ -5,15 +5,15 @@ import java.io.Serializable
 
 data class FoodItem(
     val name: String,
-    val calories: Double,
-    val serving_size_g: Double,
-    val fat_total_g: Double,
-    val fat_saturated_g: Double,
+    var calories: Double,
+    var serving_size_g: Double,
+    var fat_total_g: Double,
+//    val fat_saturated_g: Double,
     val protein_g: Double,
-    val sodium_mg: Int,
-    val potassium_mg: Int,
-    val cholesterol_mg: Int,
+//    val sodium_mg: Int,
+//    val potassium_mg: Int,
+//    val cholesterol_mg: Int,
     val carbohhydrates_total_g: Double,
-    val fiber_g: Double,
-    val sugar_g: Double
+//    val fiber_g: Double,
+//    val sugar_g: Double
 ): Serializable
